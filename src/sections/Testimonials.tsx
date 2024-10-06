ed0316f0ec7c1592eff2256fc615f3a369463814
SumitGarg <garg10sumit@gmail.com>
2024-10-07 02:11:14 +0530

    commit

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -5,7 +5,8 @@ import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
 import { SectionHeader } from "@/components/SectionHeader";
 import Image from "next/image";
-
+import grainImage from "@/assets/images/grain.jpg";
+import { Card } from "@/components/Card";
 const testimonials = [
   {
     name: "Alex Turner",
@@ -41,19 +42,23 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-    <div>
-      <SectionHeader eyebrow="Happy Clients"  title="What Clients Say About Me " 
-      description=" Don't just take my word for it. See what my clients have to say about my work."
-      />
-      <div>
-        {testimonials.map(testimonials => (
-          <div key = {testimonials.name}> 
-            <Image  src = {testimonials.avatar} alt={testimonials.name} />
-            <div>{testimonials.name}</div>
-            <div>{testimonials.position}</div>
-            <p>{testimonials.text}</p>
-          </div>
-        ))}
+    <div className="py-16">
+      <div className="container">
+        <SectionHeader
+          eyebrow="Happy Clients"
+          title="What Clients Say About Me "
+          description=" Don't just take my word for it. See what my clients have to say about my work."
+        />
+        <div>
+          {testimonials.map((testimonials) => (
+            <Card key={testimonials.name}>
+              <Image src={testimonials.avatar} alt={testimonials.name} />
+              <div>{testimonials.name}</div>
+              <div>{testimonials.position}</div>
+              <p>{testimonials.text}</p>
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
   );
